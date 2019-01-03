643ec8ee81ce20f53b4e4b94f64352e479622713
Ulrich Schmidt <admin@deruli.de>
2019-01-03 07:31:07 +0100

    use only functions

--- src/content/modules/odd_and_even/tests/OddAndEvenTest.php
+++ src/content/modules/odd_and_even/tests/OddAndEvenTest.php
@@ -1,48 +1,50 @@
 <?php
-use OddAndEven;
+use function OddAndEven\isEven;
+use function OddAndEven\isOdd;
+
 class OddAndEvenTest extends \PHPUnit\Framework\TestCase {
 	public function testIsEvenWithIntegerExpectTrue() {
-		$this->assertTrue ( OddAndEven\isEven ( 2 ) );
-		$this->assertTrue ( OddAndEven\isEven ( 56 ) );
-		$this->assertTrue ( OddAndEven\isEven ( 100 ) );
+		$this->assertTrue ( isEven ( 2 ) );
+		$this->assertTrue ( isEven ( 56 ) );
+		$this->assertTrue ( isEven ( 100 ) );
 	}
 	public function testIsEvenWithIntegerExpectFalse() {
-		$this->assertFalse ( OddAndEven\isEven ( 1 ) );
-		$this->assertFalse ( OddAndEven\isEven ( 13 ) );
+		$this->assertFalse ( isEven ( 1 ) );
+		$this->assertFalse ( isEven ( 13 ) );
 	}
 	public function testIsOddWithIntegerExpectTrue() {
-		$this->assertTrue ( OddAndEven\isOdd ( 1 ) );
-		$this->assertTrue ( OddAndEven\isOdd ( 13 ) );
-		$this->assertTrue ( OddAndEven\isOdd ( pi () ) );
+		$this->assertTrue ( isOdd ( 1 ) );
+		$this->assertTrue ( isOdd ( 13 ) );
+		$this->assertTrue ( isOdd ( pi () ) );
 	}
 	public function testIsOddWithIntegerExpectFalse() { //
-		$this->assertFalse ( OddAndEven\isOdd ( 2 ) );
-		$this->assertFalse ( OddAndEven\isOdd ( 56 ) );
-		$this->assertFalse ( OddAndEven\isOdd ( 100 ) );
+		$this->assertFalse ( isOdd ( 2 ) );
+		$this->assertFalse ( isOdd ( 56 ) );
+		$this->assertFalse ( isOdd ( 100 ) );
 	}
 	public function testIsEvenWithFloatExpectTrue() {
-		$this->assertTrue ( OddAndEven\isEven ( 1.99 ) );
-		$this->assertTrue ( OddAndEven\isEven ( 5.95 ) );
-		$this->assertTrue ( OddAndEven\isEven ( 6.10 ) );
+		$this->assertTrue ( isEven ( 1.99 ) );
+		$this->assertTrue ( isEven ( 5.95 ) );
+		$this->assertTrue ( isEven ( 6.10 ) );
 	}
 	public function testIsEvenWithFloatExpectFalse() {
-		$this->assertFalse ( OddAndEven\isEven ( 0.99 ) );
-		$this->assertFalse ( OddAndEven\isEven ( 8.99 ) );
-		$this->assertFalse ( OddAndEven\isEven ( 18.95 ) );
+		$this->assertFalse ( isEven ( 0.99 ) );
+		$this->assertFalse ( isEven ( 8.99 ) );
+		$this->assertFalse ( isEven ( 18.95 ) );
 	}
 	public function testIsOddWithFloatExpectTrue() {
-		$this->assertTrue ( OddAndEven\isOdd ( 1.12 ) );
-		$this->assertTrue ( OddAndEven\isOdd ( 11.05 ) );
-		$this->assertTrue ( OddAndEven\isOdd ( pi () ) );
+		$this->assertTrue ( isOdd ( 1.12 ) );
+		$this->assertTrue ( isOdd ( 11.05 ) );
+		$this->assertTrue ( isOdd ( pi () ) );
 	}
 	public function testIsOddWithFloatExpectFalse() { //
-		$this->assertFalse ( OddAndEven\isOdd ( 2.10 ) );
-		$this->assertFalse ( OddAndEven\isOdd ( 56.42 ) );
-		$this->assertFalse ( OddAndEven\isOdd ( 100 ) );
+		$this->assertFalse ( isOdd ( 2.10 ) );
+		$this->assertFalse ( isOdd ( 56.42 ) );
+		$this->assertFalse ( isOdd ( 100 ) );
 	}
 	public function testIsEvenWithInvalidArgument() {
 		try {
-			OddAndEven\isOdd ( "not a number" );
+			isOdd ( "not a number" );
 			$this->fail ( "Expected exception not thrown" );
 		} catch ( Exception $e ) {
 			$this->assertInstanceOf ( \InvalidArgumentException::class, $e );
@@ -50,7 +52,7 @@ class OddAndEvenTest extends \PHPUnit\Framework\TestCase {
 	}
 	public function testIsOddWithInvalidArgument() {
 		try {
-			OddAndEven\isEven ( "not a number" );
+			isEven ( "not a number" );
 			$this->fail ( "Expected exception not thrown" );
 		} catch ( Exception $e ) {
 			$this->assertInstanceOf ( \InvalidArgumentException::class, $e );
